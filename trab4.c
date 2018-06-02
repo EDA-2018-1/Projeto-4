739d83ddf3c0973823aa46bfad7253638ad4f60c
Ícaro <icarooliv2@gmail.com>
2018-06-02 19:06:32 -0300

    adds randomize

--- trab4.c
+++ trab4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<time.h>
+#include<unistd.h> 
 
 typedef struct aviao{
     char codAviao[7];
@@ -23,31 +24,58 @@ typedef struct filaDecolagem{
     Lista *ini;
     Lista *fim;
 } FilaDecolagem;
-
-int main(){
-
-    // FILE *fp;
-    // char nomeArquivo[] = "lista_voos.txt";
-
-    // fp = fopen(nomeArquivo, "r");
-    // if (fp == NULL) {
-    //     printf("Falha ao abrir o arquivo.\n");
-    //     exit(1);
-    // }
-
-    // while (!feof(fp)) {
-    //     //
-    // }
-
-    // fclose(fp);
-    // return 0;
-
-    int c;
-    FILE *file;
-    file = fopen("lista_voos.txt", "r");
-    if (file) {
-        while ((c = getc(file)) != EOF)
-            putchar(c);
-        fclose(file);
+ 
+int randomize(int lower, int upper, 
+                             int count)
+{
+    int i;
+    for (i = 0; i < count; i++) {
+        int num = (rand() %
+           (upper - lower + 1)) + lower;
+        return num;
     }
+}
+ 
+int main()
+{
+	printf("Sorteando...");
+	int nVoos = randomize(20, 64, 1); 	
+	
+	srand((unsigned)time(NULL));
+	int nAprox = randomize(10, 32, 1);
+
+	int isValid = 0;
+	while(isValid == 0){
+		if (nAprox > nVoos){
+			nAprox = randomize(10, 32, 1);
+		}
+		else {
+			isValid = 1;
+		}
+	}
+
+	int nDecolagens = nVoos - nAprox;
+    
+    printf("\n %d \n %d \n %d \n", nVoos, nAprox, nDecolagens);
+
+    printf("Abastecendo aviões...");
+    
+    int combA[nAprox];
+    int i;
+    
+    for (i = 0; i < nAprox; i++){
+    	combA[i] = randomize(0, 12, 1);
+    }
+	
+	for (i = 0; i < nAprox; i++){
+		printf("combA[%d]: %d \n", i, combA[i]);
+    }
+ 	
+    int vooOuDecolagem[nVoos];
+
+    for(i = 0; i < nVoos; i++){
+    	vooOuDecolagem = randomize(0, 1, 0);
+    }
+
+    return 0;
 }
