5e7726aa07aab21b386c051a3c93b84406c76886
Ícaro <icarooliv2@gmail.com>
2018-06-02 21:14:22 -0300

    changes

--- trab4.c
+++ trab4.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include<time.h>
 #include<unistd.h> 
+#include <string.h>
 
 typedef struct aviao{
     char codAviao[7];
@@ -25,9 +26,7 @@ typedef struct filaDecolagem{
     Lista *fim;
 } FilaDecolagem;
  
-int randomize(int lower, int upper, 
-                             int count)
-{
+int randomize(int lower, int upper, int count){
     int i;
     for (i = 0; i < count; i++) {
         int num = (rand() %
@@ -39,9 +38,9 @@ int randomize(int lower, int upper,
 int main()
 {
 	printf("Sorteando...");
-	int nVoos = randomize(20, 64, 1); 	
-	
 	srand((unsigned)time(NULL));
+
+	int nVoos = randomize(20, 64, 1); 	
 	int nAprox = randomize(10, 32, 1);
 
 	int isValid = 0;
@@ -56,9 +55,9 @@ int main()
 
 	int nDecolagens = nVoos - nAprox;
     
-    printf("\n %d \n %d \n %d \n", nVoos, nAprox, nDecolagens);
+    printf("\n Total: %d \n Aprox: %d \n Decol: %d \n", nVoos, nAprox, nDecolagens);
 
-    printf("Abastecendo aviões...");
+    printf("Abastecendo aviões...\n");
     
     int combA[nAprox];
     int i;
@@ -71,10 +70,43 @@ int main()
 		printf("combA[%d]: %d \n", i, combA[i]);
     }
  	
-    int vooOuDecolagem[nVoos];
+char codAvioes[64][7] = {"VG3001", "JJ4404", "LN7001", "TG1501", 
+							"GL7602", "TT1010", "AZ1009", "AZ1008", 
+							"AZ1010", "TG1506", "VG3002", "JJ4402", 
+							"GL7603", "RL7880", "AL0012", "TT4544", 
+							"TG1505", "VG3003", "JJ4403", "JJ4401", 
+							"LN7002", "AZ1002", "AZ1007", "GL7604", 
+							"AZ1006", "TG1503", "AZ1003", "JJ4403", 
+							"AZ1001", "LN7003", "AZ1004", "TG1504", 
+							"AZ1005", "TG1502", "GL7601", "TT4500", 
+							"RL7801", "JJ4410", "GL7607", "AL0029", 
+							"VV3390", "VV3392", "GF4681", "GF4690", 
+							"AZ1020", "JJ4435", "VG3010", "LF0920", 
+							"AZ1065", "LF0978", "RL7867", "TT4502", 
+							"GL7645", "LF0932", "JJ4434", "TG1510", 
+							"TT1020", "AZ1098", "BA2312", "VG3030", 
+							"BA2304", "KL5609", "KL5610", "KL5611"};
 
-    for(i = 0; i < nVoos; i++){
-    	vooOuDecolagem = randomize(0, 1, 0);
+    char* voosDecolagem = (char*)calloc(sizeof(char)*nDecolagens);
+    // char voosAprox[nAprox][7];
+
+    for (i = 0; i < 1; i++){
+		printf("%d \n", i);
+    	int temp = randomize(0, 64, 1);
+    	int isValid = 0;
+		strcpy(voosDecolagem[i], codAvioes[temp]); 
+
+    	for(int j = 0; j < nDecolagens; j++){
+    		printf("%s\n", voosDecolagem[j]);
+	    	if(strcmp(voosDecolagem[j], codAvioes[temp]) == 0){ 
+				temp = randomize(0, 64, 1);	  
+				strcpy(voosDecolagem[j], codAvioes[temp]); 
+	    	}
+    	}
+    }
+
+    for (i = 0; i < nDecolagens; i++){
+    	printf("decolagem[%d] %s\n", i+1, voosDecolagem[i]);
     }
 
     return 0;
